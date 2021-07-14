5297c50740db35e2fabff54336046ea91bb68f25
Star Bist <malimirkeccita@gmail.com>
2021-07-14 13:17:02 +0200

    Switching to ButtonDown.

--- scripts/index.js
+++ scripts/index.js
@@ -13,15 +13,12 @@ try {
   if(doc[0].list && doc[0].list.length) {
     doc[0].list.forEach(item => {
       list1 += `
-    <li>
-      <p>
-        <span><a href="${item.slink}" target="_blank" style="text-decoration:none"><strong style="color:#FF3366">${item.title}</strong><br><small style="color:#FF3366">${item.link}</small></a></span><br>
-        <span>${item.desc}</span>
-      </p>
-    </li>`
+      <li>
+        <p><span><a href="${item.link}" target="_blank" style="text-decoration:none"><strong style="color:#FF3366">${item.title}</strong><br><small style="color:#FF3366">${item.link}</small></a></span><br><span>${item.desc}</span></p>
+      </li>`
 
       list2 += `
-- [${item.title}](${item.slink})
+- [${item.title}](${item.link})
 ${item.desc}
 
 `
@@ -41,19 +38,14 @@ ${item.link} ${!item.handle ? '' : item.handle.indexOf('@') !== -1 ? 'via ' + it
   console.log('~~~~~~~~~~~~~')
 
   console.log(`
-<a style="display:block;background:#eee;padding:2rem;text-decoration:none" href="${doc[0].slink}"><img alt="UI Dev Newsletter logo" class="tl-email-image" height="100" src="https://res.cloudinary.com/starbist/image/upload/v1619351897/uidevnewsletter-logo3_uhcdjf.png" style="margin: 0 auto; border: none; width: 400px; max-width: 400px;" width="400" /></a>
-</a>
-
-<div style="background:#fff;border:1px solid #eee;padding:2rem">
-<h1><a href="${doc[0].slink}" target="_blank" style="text-decoration:none"><strong style="color:#FF3366">Issue #${doc.length}</strong></a></h1>
-
-<h5><strong>${doc[0].date}</strong></h5>
-
-<ul style="list-style:none;padding:0">
-  ${list1}
-</ul>
-
-<p>Happy coding!</p>
+<p style="margin:0">
+  <a style="display:block;background:#eee;padding:2rem;text-decoration:none" href="${doc[0].link}"><img alt="UI Dev Newsletter logo" class="tl-email-image" height="100" src="https://res.cloudinary.com/starbist/image/upload/v1619351897/uidevnewsletter-logo3_uhcdjf.png" style="margin: 0 auto;border: none;width: 400px;max-width: 400px;" width="400" /></a>
+</p>
+<div style="background:#fff;border:1px solid #eee;padding:2rem;text-align:left;">
+  <h1><a href="${doc[0].link}" target="_blank" style="text-decoration:none"><strong style="color:#FF3366">Issue #${doc.length}</strong></a></h1>
+  <h5><strong>${doc[0].date}</strong></h5>
+  <ul style="list-style:none;padding:0;margin:0">${list1}</ul>
+  <p>Happy coding!</p>
 </div>
 `);
 
@@ -66,7 +58,7 @@ ${list2}
 
 Happy coding!
 
-[Subscribe to the newsletter here!](https://bit.ly/34155z3)
+[Subscribe to the newsletter here!](https://buttondown.email/starbist)
   `);
 
 
@@ -92,7 +84,7 @@ https://mentor.silvestar.codes/reads/${doc[0].date}/
 Featuring ${doc[0].list.map(d => d.handle).join(' ')}
 `)
 
-  if(!doc[0].slink || doc[0].slink === '') {
+  if(!doc[0].link || doc[0].link === '') {
     console.error('\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nShorten the links!!!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
   }
 
